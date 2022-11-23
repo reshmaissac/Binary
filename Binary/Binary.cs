764022cd965c89d1e83b1ace8fdb8e46cf12d402
reshmaissac <reshmaissac2012@gmail.com>
2022-11-23 11:33:06 +0000

    Add project files.

---
+++ Binary/Binary.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CP
+{
+    // Requirements, Assignment/Implementation Rules:
+    //  You must work with 16 bits, hint: Use an int array of 16 as a field
+    //  Binary operators: ~, <<, >>
+    //  Binary, Arithmatic, and Logical operators: +, -, *, /, ==, !=, <, >, <=, >= algorithms
+    //      must perform in binary. Algorthims that convert to denary, perform in denary, and convert
+    //      back to binary carry 0 marks.
+
+    class Binary
+    {
+        #region(Fields)
+        #endregion
+        #region(Properties)
+        #endregion
+        #region(Index operator)
+        #endregion
+        #region(Implicit Convertors: int to Binary, Binary to int)
+        #endregion
+        #region(Methods: ToDecimal, ToString)
+        #endregion
+        #region(Shift Opertors: Shift to left by n (<<), Shift to right by n (>>))
+        #endregion
+        #region(Binary Operators: Ones' complement, Negation)
+        #endregion
+        #region(Binary Arithmatic Opertors: +, -, *, /)
+        #endregion
+        #region(Logical Operators: ==, !=, <, >, <=, >=)
+        #endregion
+    }
+}
