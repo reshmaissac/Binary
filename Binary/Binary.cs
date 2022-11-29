3476e75b14e8061c6928c580abc4caee67342b8d
reshmaissac <reshmaissac2012@gmail.com>
2022-11-29 13:36:17 +0000

    Added try catch

--- Binary/Binary.cs
+++ Binary/Binary.cs
@@ -22,46 +22,62 @@ namespace CP
 
         public static implicit operator Binary(int num)
         {
-            int positiveNum = Math.Abs(num);
-            int[] array = new int[16];
-            Binary bin = new Binary();
-            //create int array in binary form
-            for (int i = 15; positiveNum > 0; i--)
-            {
-                array[i] = positiveNum % 2;
-                positiveNum = positiveNum / 2;
-            }
-            bin.value = array;
-            //binary of a -ve num is equivalent to 2s complement of its +ve value.
-            if (num < 0)
+            try
             {
 
-                bin = -bin;
+                int positiveNum = Math.Abs(num);
+                int[] array = new int[16];
+                Binary bin = new Binary();
+                //create int array in binary form
+                for (int i = 15; positiveNum > 0; i--)
+                {
+                    array[i] = positiveNum % 2;
+                    positiveNum = positiveNum / 2;
+                }
+                bin.value = array;
+                //binary of a -ve num is equivalent to 2s complement of its +ve value.
+                if (num < 0)
+                {
+
+                    bin = -bin;
+                }
+                return bin;
             }
-            return bin;
+            catch
+            {
+                return null;
+            }
 
         }
 
         public static implicit operator int(Binary bin)
         {
-            int num = 0, power = 1;
-            int[] array2 = bin.value;
-            //Find int of signed binary.(If 1st bit  is 1 its -ve integer, else +ve integer)
-            if (array2[0] == 1)
+            try
             {
-                Binary bin1 = ~bin;
-                bin1.value.CopyTo(array2, 0);
+                int num = 0, power = 1;
+                int[] array2 = bin.value;
+                //Find int of signed binary.(If 1st bit  is 1 its -ve integer, else +ve integer)
+                if (array2[0] == 1)
+                {
+                    Binary bin1 = ~bin;
+                    bin1.value.CopyTo(array2, 0);
 
-                AddOneToArray(array2);
+                    AddOneToArray(array2);
 
-                power = -1;
+                    power = -1;
+                }
+                for (int i = (array2.Length - 1); i >= 0; i--)
+                {
+                    num = num + (array2[i] * power);
+                    power = power * 2;
+                }
+                return num;
+
             }
-            for (int i = (array2.Length - 1); i >= 0; i--)
+            catch
             {
-                num = num + (array2[i] * power);
-                power = power * 2;
+                return 0;
             }
-            return num;
         }
 
         #endregion
@@ -69,19 +85,27 @@ namespace CP
 
         public override string ToString()
         {
-            int[] array = this.value;
-            string binaryString = "";
-            for (int i = 0; i < array.Length; i++)
+            try
             {
-                binaryString = binaryString + array[i];
-                if ((i + 1) % 4 == 0)
+                int[] array = this.value;
+                string binaryString = "";
+                for (int i = 0; i < array.Length; i++)
                 {
-                    binaryString = binaryString + " ";
+                    binaryString = binaryString + array[i];
+                    if ((i + 1) % 4 == 0)
+                    {
+                        binaryString = binaryString + " ";
+                    }
                 }
+                //string binaryString = string.Join(string.Empty, this.value);
+                //return string.Join(' ', binaryString.Chunk(size: 4).Select(b => new string(b)));
+                return binaryString;
+
             }
-            //string binaryString = string.Join(string.Empty, this.value);
-            //return string.Join(' ', binaryString.Chunk(size: 4).Select(b => new string(b)));
-            return binaryString;
+            catch
+            {
+                return string.Empty;
+            }
         }
 
         public decimal ToDecimal()
@@ -107,64 +131,88 @@ namespace CP
 
         public static Binary operator <<(Binary binary, int n)
         {
-            int[] array = binary.value;
-            for (int i = 0; i < array.Length; i++)
+            try
             {
-                if ((i - n) >= 0)
+                int[] array = binary.value;
+                for (int i = 0; i < array.Length; i++)
                 {
-                    array[i - n] = array[i];
+                    if ((i - n) >= 0)
+                    {
+                        array[i - n] = array[i];
+                    }
                 }
+                // Add 0 to rightmost n bits
+                for (int j = (array.Length - 1); j >= (array.Length - n); j--)
+                {
+                    array[j] = 0;
+                }
+                binary.value = array;
+                return binary;
+
             }
-            // Add 0 to rightmost n bits
-            for (int j = (array.Length - 1); j >= (array.Length - n); j--)
+            catch(Exception e)
             {
-                array[j] = 0;
+                Console.WriteLine(e.Message);
+                return null;
             }
-            binary.value = array;
-            return binary;
         }
         public static Binary operator >>(Binary binary, int n)
         {
-
-            int[] array = binary.value;
-            for (int i = (array.Length - 1); i >= 0; i--)
+            try
             {
-                if ((i + n) < array.Length)
+                int[] array = binary.value;
+                for (int i = (array.Length - 1); i >= 0; i--)
                 {
-                    array[i + n] = array[i];
+                    if ((i + n) < array.Length)
+                    {
+                        array[i + n] = array[i];
+                    }
                 }
+                //Add 0 to the leftmost n bits
+                for (int j = 0; j < n; j++)
+                {
+                    array[j] = 0;
+                }
+                binary.value = array;
+                return binary;
+
             }
-            //Add 0 to the leftmost n bits
-            for (int j = 0; j < n; j++)
+            catch
             {
-                array[j] = 0;
+                return null;
             }
-            binary.value = array;
-            return binary;
         }
         #endregion
         #region(Binary Operators: Ones' complement, Negation)
 
         public static Binary operator ~(Binary binary)
         {
-            Binary binary2 = new Binary();
-            int[] array = new int[16];
-            binary.value.CopyTo(array, 0);
-
-            for (int i = 0; i < array.Length; i++)
+            try
             {
-                if (array[i] == 0)
-                {
-                    array[i] = 1;
+                Binary binary2 = new Binary();
+                int[] array = new int[16];
+                binary.value.CopyTo(array, 0);
 
-                }
-                else
+                for (int i = 0; i < array.Length; i++)
                 {
-                    array[i] = 0;
+                    if (array[i] == 0)
+                    {
+                        array[i] = 1;
+
+                    }
+                    else
+                    {
+                        array[i] = 0;
+                    }
                 }
+                binary2.value = array;
+                return binary2;
+
             }
-            binary2.value = array;
-            return binary2;
+            catch
+            {
+                return null;
+            }
         }
         public static Binary operator -(Binary binary)
         {
